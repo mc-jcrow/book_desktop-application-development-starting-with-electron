311865ffecb3ead8e59c5a117f212e3b70ceb247
Your Name <you@example.com>
2023-08-10 13:43:04 +0900

    記載コードが古く動作しなかったため、コードを修正

---
+++ main.js
@@ -0,0 +1,140 @@
+const { app, Menu, MenuItem, BrowserView, BrowserWindow, ipcMain } = require('electron');
+const path = require('path');
+
+const win_name = [
+  'banana', 'orange', 'apple'
+];
+
+// レンダラープロセスから「hello」チャンネルへメッセージが届いたとき、
+// ウィンドウを開き、レンダラープロセスへメッセージを返す。
+ipcMain.handle('hello', (event, arg) => {
+  const result = createWindow();
+  const text = win_name[result % 3] + '-' + result;
+  return text;
+});
+
+function createWindow() {
+    win = new BrowserWindow ({
+        width: 1000,
+        height: 800,
+        webPreferences: {
+          preload: path.join(__dirname, 'preload.js')
+        }
+    });
+    win.loadFile('index.html');
+    win.webContents.openDevTools();
+    return win.id;
+}
+
+function createMenu() {
+    const isMac = process.platform === 'darwin'
+
+    const template = [
+      // { role: 'appMenu' }
+      ...(isMac
+        ? [{
+            label: app.name,
+            submenu: [
+              { role: 'about' },
+              { type: 'separator' },
+              { role: 'services' },
+              { type: 'separator' },
+              { role: 'hide' },
+              { role: 'hideOthers' },
+              { role: 'unhide' },
+              { type: 'separator' },
+              { role: 'quit' }
+            ]
+          }]
+        : []),
+      // { role: 'fileMenu' }
+      {
+        label: 'File',
+        submenu: [
+          isMac ? { role: 'close' } : { role: 'quit' }
+        ]
+      },
+      // { role: 'editMenu' }
+      {
+        label: 'Edit',
+        submenu: [
+          { role: 'undo' },
+          { role: 'redo' },
+          { type: 'separator' },
+          { role: 'cut' },
+          { role: 'copy' },
+          { role: 'paste' },
+          ...(isMac
+            ? [
+                { role: 'pasteAndMatchStyle' },
+                { role: 'delete' },
+                { role: 'selectAll' },
+                { type: 'separator' },
+                {
+                  label: 'Speech',
+                  submenu: [
+                    { role: 'startSpeaking' },
+                    { role: 'stopSpeaking' }
+                  ]
+                }
+              ]
+            : [
+                { role: 'delete' },
+                { type: 'separator' },
+                { role: 'selectAll' }
+              ])
+        ]
+      },
+      // { role: 'viewMenu' }
+      {
+        label: 'View',
+        submenu: [
+          { role: 'reload' },
+          { role: 'forceReload' },
+          { role: 'toggleDevTools' },
+          { type: 'separator' },
+          { role: 'resetZoom' },
+          { role: 'zoomIn' },
+          { role: 'zoomOut' },
+          { type: 'separator' },
+          { role: 'togglefullscreen' }
+        ]
+      },
+      // { role: 'windowMenu' }
+      {
+        label: 'Window',
+        submenu: [
+          { role: 'minimize' },
+          { role: 'zoom' },
+          ...(isMac
+            ? [
+                { type: 'separator' },
+                { role: 'front' },
+                { type: 'separator' },
+                { role: 'window' }
+              ]
+            : [
+                { role: 'close' }
+              ])
+        ]
+      },
+      {
+        role: 'help',
+        submenu: [
+          {
+            label: 'Learn More',
+            click: async () => {
+              const { shell } = require('electron')
+              await shell.openExternal('https://electronjs.org')
+            }
+          }
+        ]
+      }
+    ]
+    
+    const menu = Menu.buildFromTemplate(template)
+    Menu.setApplicationMenu(menu)
+}
+createMenu();
+
+app.whenReady().then(createWindow);
