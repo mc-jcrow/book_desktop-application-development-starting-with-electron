7d343eb9d40d0cd483ece2cddad366f3f22678fa
admin <22795886+mc-yw@users.noreply.github.com>
2023-09-02 19:50:28 +0900

    ディレクトリ整理のためファイルを移動

--- preload.js
+++
@@ -1,9 +0,0 @@
-// プリロード (隔離ワールド)
-const { contextBridge, ipcRenderer } = require('electron');
-
-contextBridge.exposeInMainWorld(
-    'electron',
-    {
-        hello: (arg) => ipcRenderer.invoke('hello', arg)
-    }
-);
