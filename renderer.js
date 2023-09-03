caa7b7aebd770b730e0f056e1494efbebb01cbc4
admin <22795886+mc-yw@users.noreply.github.com>
2023-09-03 12:16:18 +0900

    ディレクトリ内のファイルを一部chapter4to5へ移動

--- renderer.js
+++
@@ -1,8 +0,0 @@
-async function doit() {
-    // preload.jsに定義したAPI（hello）でメインプロセスと通信する。
-    // hello()内で使われている「ipcRenderer.invoke()」は非同期関数のため、
-    // 「async」「await」を使う必要がある。
-    const res = await window.electron.hello();
-    
-    document.querySelector('#msg').textContent = `create window ${res}`;
-}
